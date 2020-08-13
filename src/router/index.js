75b78fcca3ef4b96faee7144f4551565780d2fb6
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-13 10:49:41 +0000

    chat component

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,17 @@ const routes = [{
   {
     path: '/chat',
     name: 'Chat',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import( /* webpackChunkName: "about" */ '../views/Chat.vue'),
-    props: true
+    props: true,
+    beforeEnter: (to, from, next) => {
+      if (to.params.name) {
+        next()
+      } else {
+        next({
+          name: 'Home'
+        })
+      }
+    }
   }
 ]
 
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class=" container">
-    <h2>Chat {{ this.name }}</h2>
+  <div class="chat container">
+    <h2 class="center white-text">Vee chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="message">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3"> message</span>
+            <div class="grey-text time">time</div>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <input type="text" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,3 +27,17 @@ export default {
   },
 };
 </script>
+<style scoped>
+h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+span {
+  font-size: 1.4em;
+}
+
+.time {
+  display: block;
+  font-size: 1.2em;
+}
+</style>
