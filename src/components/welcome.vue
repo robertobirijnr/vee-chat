1b75959e2d0c78618febd56e14be95042bf14481
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-13 01:39:02 +0000

    welcome page

---
+++ src/components/welcome.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name" />
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    enterChat() {
+      console.log(this.name);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.welcome {
+  max-width: 400px;
+  margin-top: 100px;
+}
+
+h2 {
+  font-size: 3em;
+}
+
+button {
+  margin: 30px auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <welcome />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import welcome from "@/components/welcome";
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    welcome,
+  },
+};
 </script>
+
+<style>
+body {
+  background: teal;
+}
+</style>
