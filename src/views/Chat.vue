54140ab0eae9be193c92da26b77b8057749a044d
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-13 02:17:11 +0000

    chat component

---
+++ src/views/Chat.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class=" container">
+    <h2>Chat {{ this.name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "chat",
+  props: ["name"],
+  data() {
+    return {};
+  },
+};
+</script>
